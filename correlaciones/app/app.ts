06ddbc5732d269be6ea60f4d065e8f70cb86fa72
Alexis <alexisvt@gmail.com>
2016-07-11 16:24:08 -0600

    adding css to app and adding a reference to correlations page for testing

--- correlaciones/app/app.ts
+++ correlaciones/app/app.ts
@@ -1,2 +1,5 @@
 ﻿import * as application from "application";
-application.start({ moduleName: "pages/main-page/main-page" });
+
+application.cssFile = 'styles/app.css';
+// application.start({ moduleName: "pages/main-page/main-page" });
+application.start({ moduleName: "pages/correlations-page/correlations-page" });
