9afa20b3a69463a829d6657786512f7f7d32cea2
Alexis <alexisvt@gmail.com>
2016-07-12 08:18:02 -0600

    adding fix to prevent to read non existing file

--- correlaciones/app/services/correlation-service.ts
+++ correlaciones/app/services/correlation-service.ts
@@ -2,25 +2,31 @@ import { Correlation } from '../shared';
 import * as FSModule from 'file-system';
 
 const DOCUMENTS = FSModule.knownFolders.currentApp();
-const CORRELATIONS_FILE = DOCUMENTS.getFile('data/correlations-es.json');
+const CORRELATIONS_FILE = DOCUMENTS.getFile('data/correlations-es,json');
 
 export function showAllCorrelations(): Promise<Correlation[]> {
 
   let jsonData: Array<Correlation> = [];
+  const filePath = FSModule.path.join(DOCUMENTS.path, CORRELATIONS_FILE.name);
+  if (FSModule.File.exists(filePath)) {
+    return CORRELATIONS_FILE.readText()
+      .then(function onFulfilled(content: string) {
+        try {
+          jsonData = <Array<Correlation>>JSON.parse(content);
+        } catch (error) {
+          return Promise.reject(`Couldn't parse the file for this reason: ${error}`);
+        }
+        return Promise.resolve(jsonData)
+      },
+      function (message: any) {
+        console.log(message);
+        return Promise.reject(`Couldn't load the file for this reason: ${message}`);
+      });
 
-  return CORRELATIONS_FILE.readText()
-    .then(function onFulfilled(content: string) {
-      try {
-        jsonData = <Array<Correlation>>JSON.parse(content);
-      } catch (error) {
-        return Promise.reject(`Couldn't parse the file for this reason: ${error}`);
-      }
-      return Promise.resolve(jsonData)
-    },
-    function (message: any) {
-      console.log(message);
-      return Promise.reject(`Couldn't load the file for this reason: ${message}`);
-    });
+  } else {
+    console.log('non existing file');
+    return Promise.reject('non existing file');
+  }
 }
 
 export function searchByOldNumber(oldHimnNumber: number): Promise<Correlation> {
