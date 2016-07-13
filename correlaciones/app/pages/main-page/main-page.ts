1bf1ef4fa86813fb82e3d9d5fdb869c8aa3f9733
Alexis <alexisvt@gmail.com>
2016-07-13 10:11:19 -0600

    improving search logic

--- correlaciones/app/pages/main-page/main-page.ts
+++ correlaciones/app/pages/main-page/main-page.ts
@@ -2,6 +2,7 @@ import { EventData, Observable } from 'data/observable';
 import { Page } from 'ui/page';
 import MainViewModel from './main-view-model';
 import { TextField } from 'ui/text-field';
+import { Switch } from 'ui/switch';
 
 let page: Page;
 let vm = new MainViewModel();
@@ -9,11 +10,4 @@ let vm = new MainViewModel();
 export function onLoaded(args: EventData) {
   page = <Page>args.object;
   page.bindingContext = vm;
-
-  // attaching an eventhanlder to listen changes on the field value
-  let oldHimnTextField = <TextField>page.getViewById('txtOld');
-  oldHimnTextField.on(Observable.propertyChangeEvent, vm.searchByOldHimnNum.bind(vm))
-
-  let newHimnTextField = <TextField>page.getViewById('txtNew');
-  newHimnTextField.on(Observable.propertyChangeEvent, vm.searchByNewHimnNum.bind(vm));
 }
--- correlaciones/app/pages/main-page/main-view-model.ts
+++ correlaciones/app/pages/main-page/main-view-model.ts
@@ -2,13 +2,15 @@ import { Observable, EventData, PropertyChangeData } from 'data/observable';
 import { alert, AlertOptions } from 'ui/dialogs';
 import { Correlation } from '../../shared/correlation';
 import * as CorrelationService from '../../services/correlation-service';
+import { GestureEventData } from 'ui/gestures';
+import { TextField } from 'ui/text-field';
 
 export default class MainViewModel extends Observable {
 
-  himnName = '';
-  oldNum = 0;
-  newNum = 0;
-  searchByNewHimn = false;
+  private _himnName = '';
+  private _oldHimnNum: number | string = 0;
+  private _newHimnNum: number | string = 0;
+  private _enableSearchBynewHimnNum = false;
 
   constructor() {
     super();
@@ -18,19 +20,91 @@ export default class MainViewModel extends Observable {
 
   }
 
-  private cleanSearchFields(): void {
-    this.set('oldNum', 0);
-    this.set('newNum', 0);
-    this.set('himnName', '');
+  // getter and setter
+
+  public get himnName(): string {
+    return this._himnName;
   }
 
-  searchByOldHimnNum(args: PropertyChangeData): void {
+  public set himnName(v: string) {
 
-    if (args.value && args.value > 0 && !this.searchByNewHimn) {
-      CorrelationService.searchByOldNumber(+args.value)
+    if (this._himnName != v) {
+      this._himnName = v;
+      this.notify({ object: this, eventName: Observable.propertyChangeEvent, propertyName: 'himnName', value: v });
+    }
+  }
+
+  public get oldHimnNum(): number | string {
+    return this._oldHimnNum;
+  }
+
+  public set oldHimnNum(v: number | string) {
+
+    this._oldHimnNum = v;
+    this.notify({ object: this, eventName: Observable.propertyChangeEvent, propertyName: 'oldHimnNum', value: v });
+    this.searchByOldHimnNum();
+
+  }
+
+  public get newHimnNum(): number | string {
+    return this._newHimnNum;
+  }
+
+  public set newHimnNum(v: number | string) {
+
+    this._newHimnNum = v;
+    this.notify({ object: this, eventName: Observable.propertyChangeEvent, propertyName: 'newHimnNum', value: v });
+    this.searchByNewHimnNum();
+
+  }
+
+  public get enableSearchBynewHimnNum(): boolean {
+    return this._enableSearchBynewHimnNum;
+  }
+
+  public set enableSearchBynewHimnNum(v: boolean) {
+    if (this._enableSearchBynewHimnNum != (!!v)) {
+      this._enableSearchBynewHimnNum = !!v;
+      this.notify({ object: this, eventName: Observable.propertyChangeEvent, propertyName: 'enableSearchBynewHimnNum', value: 0 });
+
+      this.cleanSearchFields();
+    }
+  }
+
+  // methods
+  cleanNumField(args: GestureEventData): void {
+
+    let textField = <TextField>args.view;
+    console.log(textField.id);
+    
+    if (textField.id && textField.id === 'txtOld') {
+      this.oldHimnNum = '';
+    } else if (textField.id && textField.id === 'txtNew') {
+      this.newHimnNum = '';
+    }
+
+  }
+
+  onSwitchPropertyChange(): void {
+    this.cleanSearchFields();
+  }
+
+  // methods
+  private cleanSearchFields(): void {
+
+    this.oldHimnNum = 0;
+    this.newHimnNum = 0;
+    this.himnName = '';
+
+  }
+
+  private searchByOldHimnNum(): void {
+
+    if (this._oldHimnNum && this._oldHimnNum > 0 && !this._enableSearchBynewHimnNum) {
+      CorrelationService.searchByOldNumber(+this._oldHimnNum)
         .then(
         (correlation: Correlation) => {
-          this.set('newNum', correlation.newNum);
+          this.set('newHimnNum', correlation.newNum);
           this.set('himnName', correlation.name);
         },
         (message: any) => {
@@ -49,13 +123,13 @@ export default class MainViewModel extends Observable {
 
   }
 
-  searchByNewHimnNum(args: PropertyChangeData): void {
+  private searchByNewHimnNum(): void {
 
-    if (args.value && args.value > 0 && this.searchByNewHimn) {
-      CorrelationService.searchByNewNumber(+args.value)
+    if (this._newHimnNum && this._newHimnNum > 0 && this._enableSearchBynewHimnNum) {
+      CorrelationService.searchByNewNumber(+this._newHimnNum)
         .then(
         (correlation: Correlation) => {
-          this.set('oldNum', correlation.oldNum);
+          this.set('oldHimnNum', correlation.oldNum);
           this.set('himnName', correlation.name);
         },
         (message: any) => {
