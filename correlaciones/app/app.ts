c0acb74db9d84186173cf1d56fe7eded1f1f9d9d
Alexis <alexisvt@gmail.com>
2016-07-25 12:49:35 -0600

    renaming correlation page to himns

--- correlaciones/app/app.ts
+++ correlaciones/app/app.ts
@@ -2,4 +2,3 @@
 
 application.cssFile = 'styles/app.css';
 application.start({ moduleName: "pages/main-page/main-page" });
-// application.start({ moduleName: "pages/correlations-page/correlations-page" });
